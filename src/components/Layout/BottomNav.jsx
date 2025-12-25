a3f8aff16d852d37b866c2f6c0806ca1085bf86c
RANJITHKUMAR <157204592+Ranjithkumar-67@users.noreply.github.com>
2025-12-25 06:32:26 +0530

    Update BottomNav.jsx

--- src/components/Layout/BottomNav.jsx
+++ src/components/Layout/BottomNav.jsx
@@ -1,1 +1,10 @@
 
+import { Home, Target, CheckCircle2, Settings } from "./Icons";
+
+export default function BottomNav({ page, setPage }) {
+  return (
+    <div className="glass-bottom-nav fixed bottom-0 left-0 right-0 max-w-lg mx-auto">
+      {/* paste your existing bottom nav JSX here */}
+    </div>
+  );
+}
